4efd1f144f52f8726ef9a94767a797091e6d0108
Leticia Sontay <lsontayc1@miumg.edu.gt>
2024-05-04 13:56:51 -0600

    inicio proyecto peliculas

---
+++ ReposicionExamen/src/main/java/com/mycompany/reposicionexamen/ReposicionExamen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reposicionexamen;
+
+/**
+ *
+ * @author alumno
+ */
+public class ReposicionExamen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
