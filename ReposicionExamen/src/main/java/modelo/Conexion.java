ced690b17257806c9b1dd223c5e623c79209678b
Leticia Sontay <lsontayc1@miumg.edu.gt>
2024-05-04 15:21:25 -0600

    configuracion conexion

--- ReposicionExamen/src/main/java/modelo/Conexion.java
+++ ReposicionExamen/src/main/java/modelo/Conexion.java
@@ -3,7 +3,7 @@
 import java.sql.*;
 
 public class Conexion {
-    private static final String JDBC_URL = "jdbc:mysql://localhost/examen1?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_URL = "jdbc:mysql://localhost/reposicion?useSSL=false&serverTimezone=UTC";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASS = "Umg$2024";
    
